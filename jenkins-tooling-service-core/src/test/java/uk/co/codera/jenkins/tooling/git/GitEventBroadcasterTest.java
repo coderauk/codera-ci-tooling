1acb9d9a1736dd24acdc6d5cb8c4be07c5ca6013
Andy Stewart <andy.stewart@codera.co.uk>
2016-04-08 18:34:10 +0100

    Added another test to ensure that any failure in one listener will not impact on the others being called

--- jenkins-tooling-service-core/src/test/java/uk/co/codera/jenkins/tooling/git/GitEventBroadcasterTest.java
+++ jenkins-tooling-service-core/src/test/java/uk/co/codera/jenkins/tooling/git/GitEventBroadcasterTest.java
@@ -2,32 +2,62 @@ package uk.co.codera.jenkins.tooling.git;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static uk.co.codera.jenkins.tooling.git.GitPushEvent.aGitPushEvent;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class GitEventBroadcasterTest {
 
-	@Test
-	public void shouldHaveNoSubscribersByDefault() {
-		assertThat(new GitEventBroadcaster().numberSubscribers(), is(0));
-	}
+    private GitEventBroadcaster broadcaster;
 
-	@Test
-	public void registeringListenerShouldIncreaseSubscriberCount() {
-		GitEventBroadcaster broadcaster = new GitEventBroadcaster();
-		broadcaster.registerListener(mock(GitEventListener.class));
-		assertThat(broadcaster.numberSubscribers(), is(1));
-	}
+    @Before
+    public void before() {
+        this.broadcaster = new GitEventBroadcaster();
+    }
 
-	@Test
-	public void shouldNotifySubscriberOfPushEvent() {
-		GitEventBroadcaster broadcaster = new GitEventBroadcaster();
-		GitEventListener listener = mock(GitEventListener.class);
-		broadcaster.registerListener(listener);
-		GitPushEvent event = GitPushEvent.aGitPushEvent().build();
-		broadcaster.onPush(event);
-		Mockito.verify(listener).onPush(event);
-	}
+    @Test
+    public void shouldHaveNoSubscribersByDefault() {
+        assertThat(this.broadcaster.numberSubscribers(), is(0));
+    }
+
+    @Test
+    public void registeringListenerShouldIncreaseSubscriberCount() {
+        registerNewListener();
+        assertThat(broadcaster.numberSubscribers(), is(1));
+    }
+
+    @Test
+    public void shouldNotifySubscriberOfPushEvent() {
+        GitEventListener listener = registerNewListener();
+        GitPushEvent event = aPushEvent();
+        broadcaster.onPush(event);
+        verify(listener).onPush(event);
+    }
+
+    @Test
+    public void anExceptionInFirstListenerShouldNotStopSubsequentListenersFromBeingCalled() {
+        GitEventListener firstListener = registerNewListener();
+        GitEventListener secondListener = registerNewListener();
+
+        doThrow(new IllegalStateException()).when(firstListener).onPush(any(GitPushEvent.class));
+        
+        broadcaster.onPush(aPushEvent());
+        
+        verify(secondListener).onPush(any(GitPushEvent.class));
+    }
+
+    private GitPushEvent aPushEvent() {
+        return aGitPushEvent().build();
+    }
+
+    private GitEventListener registerNewListener() {
+        GitEventListener listener = mock(GitEventListener.class);
+        this.broadcaster.registerListener(listener);
+        return listener;
+    }
 }

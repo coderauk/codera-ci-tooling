95ca459f794fe7c70c5af8d3e629a373745c2ee6
Andy Stewart <andy.stewart@codera.co.uk>
2016-04-19 20:53:14 +0100

    Updated to newer version of codera-lang which means the announcer can now report how many listeners it has

--- codera-ci-tooling-service-core/src/main/java/uk/co/codera/ci/tooling/git/GitEventBroadcaster.java
+++ codera-ci-tooling-service-core/src/main/java/uk/co/codera/ci/tooling/git/GitEventBroadcaster.java
@@ -1,24 +1,16 @@
 package uk.co.codera.ci.tooling.git;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import uk.co.codera.ci.tooling.git.GitEventListener;
-import uk.co.codera.ci.tooling.git.GitPushEvent;
 import uk.co.codera.lang.Announcer;
 
 public class GitEventBroadcaster implements GitEventListener {
 
-    private final Collection<GitEventListener> listeners;
     private final Announcer<GitEventListener> announcer;
 
     public GitEventBroadcaster() {
-        this.listeners = new ArrayList<>();
         this.announcer = Announcer.to(GitEventListener.class);
     }
 
     public void registerListener(GitEventListener listener) {
-        this.listeners.add(listener);
         this.announcer.addListener(listener);
     }
 
@@ -28,6 +20,6 @@ public class GitEventBroadcaster implements GitEventListener {
     }
 
     public int numberSubscribers() {
-        return this.listeners.size();
+        return this.announcer.numberListeners();
     }
 }
